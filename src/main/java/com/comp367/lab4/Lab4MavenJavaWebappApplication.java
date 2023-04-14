64a9750f45781587241b9ad0c82c698e500de800
lewisMca <124395944+lewisMca@users.noreply.github.com>
2023-04-14 11:45:20 +0100

    Add files via upload

---
+++ src/main/java/com/comp367/lab4/Lab4MavenJavaWebappApplication.java
@@ -0,0 +1,13 @@
+package com.comp367.lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4MavenJavaWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4MavenJavaWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comp367/lab4/Lab4MavenJavaWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp367.lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4MavenJavaWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
